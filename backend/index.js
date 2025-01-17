ccb8d6d69d9af3ce90534a618d4d2f24af650da8
HauwaSalihu <50779748+HauwaSalihu@users.noreply.github.com>
2025-01-17 22:46:45 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,16 @@ import { connectToDatabase } from "./config/mongodbConnection.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://ecom-project-seven-liard.vercel.app/",
+      "https://ecom-project-seven-liard.vercel.app",
+      "http://localhost:5173/",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use(express.json());
 
 app.get("/health", (req, res) => {

d760050ab3aca50a7c61d06e9f78c464f5bdd013
HauwaSalihu <50779748+HauwaSalihu@users.noreply.github.com>
2025-01-17 22:53:30 +0100

    Update helper.js
    added api/v1

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -5,4 +5,4 @@ export function formatCurrency(amount) {
   }).format(amount);
 }
 
-export const serverUrl = process.env.NODE_ENV === "production" ? "https://ecom-project-mt10.onrender.com" : "http://localhost:3000/api/v1";
+export const serverUrl = process.env.NODE_ENV === "production" ? "https://ecom-project-mt10.onrender.com/api/v1" : "http://localhost:3000/api/v1";
